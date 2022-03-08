5c3d8c9aee430c5bf7248cc9f498286a837cad80
DevelopJz <96412126+DevelopJz@users.noreply.github.com>
2022-03-08 10:03:49 +0900

    Python3 Practice
     - Python3
    입력된 변수 자동 형 변환 함수

---
+++ Classification.py
@@ -0,0 +1,51 @@
+import numpy as np
+from ast import literal_eval
+
+
+recvData=None
+
+class overload:
+ def __init__(self,inputData):
+  self.inputData=inputData
+  print(type(inputData))
+  if type(self.inputData)==str:
+   print(inputData)
+   try:
+    self.inputData=int(self.inputData)
+    print(type(self.inputData))
+   except:
+    print("정수가 아닙니다")
+    try:
+     self.inputData=float(self.inputData)
+     print(type(self.inputData))
+    except:
+     print("실수가 아닙니다")
+     try:
+      self.inputData=complex(self.inputData)
+      print(type(self.inputData))
+     except:
+      print("복소수가 아닙니다")
+      if self.inputData=="True" or self.inputData=="TRUE" or self.inputData=="true":
+       self.inputData=True
+      elif self.inputData=="False" or self.inputData=="FALSE" or self.inputData=="false":
+       self.inputData=False
+      else:
+       self.inputData=self.inputData
+   """try:
+       self.inputData=list(self.inputData.split(","))
+       print(type(self.inputData))
+      except:
+       print("리스트가 아닙니다")"""
+       
+ def switching(self):
+  return self.inputData
+
+
+
+print(type(recvData))
+
+inputData=input("아무 값이나 입력하세요 : ")
+a=overload(inputData)
+recvData=a.switching()
+print(recvData)
+print(type(recvData))
